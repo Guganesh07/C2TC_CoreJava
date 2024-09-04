0026705c63042f93b4d142cbb3e3e0ccf1e1bc21
gugan <gugan@guganesh>
2024-09-04 14:23:52 +0530

    looping

---
+++ C2TC/src/admin/gugan/project/Loops.java
@@ -0,0 +1,29 @@
+package admin.gugan.project;
+
+public class Loops {
+	public static void main(String args[]) {
+		//For loop
+		for (int i=0;i<10;i++)
+		{
+			System.out.println(i);
+		}
+		//While loop
+		int i=10;
+		while(i<=20)
+		{
+			System.out.println(i);
+			i++;
+		}
+		//Do while loop
+		i=21;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i<=30);
+
+	}
+	
+
+}
